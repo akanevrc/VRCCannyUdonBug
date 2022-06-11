ee296ae94980957567e8788036dc13bb5c72f8f0
akanevrc <akanezoranomukou@gmail.com>
2022-06-11 13:36:23 +0900

    Fix .gitognore and add Assets folder

---
+++ UdonBeforeVCC/Assets/akanevrc/UdonSharp/SampleBehaviour.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SampleBehaviour : UdonSharpBehaviour
+{
+    public int _intNumber = 0;
+    public float _floatNumber = 0.0F;
+    public Vector3 _vector = Vector3.zero;
+    public Material _material = null;
+    public UdonBehaviour _udonBehaviour = null;
+}
